b71c7729ee4268b11756a1c042870a9d806a2593
Kostya Kaganov <kkaganov@gmail.com>
2020-10-08 12:27:54 +0300

    Add validation for price dependent on accomodation type and set placeholder accordingly

--- js/form.js
+++ js/form.js
@@ -9,41 +9,66 @@
     [3]: [1, 2, 3]
   };
 
+  const PRICE_RULES_MAP = {
+    [`bungalow`]: 0,
+    [`flat`]: 1000,
+    [`house`]: 5000,
+    [`palace`]: 10000
+  };
+
   const adForm = document.querySelector(`.ad-form`);
+  const title = adForm.querySelector(`#title`);
+  const type = adForm.querySelector(`#type`);
+  const price = adForm.querySelector(`#price`);
+  const roomsNumber = adForm.querySelector(`#room_number`);
+  const guestsNumber = adForm.querySelector(`#capacity`);
 
   const enable = () => {
     adForm.classList.remove(`ad-form--disabled`);
 
-    const roomsNumber = adForm.querySelector(`#room_number`);
-    roomsNumber.addEventListener(`change`, onRoomsNumberChange);
+    title.addEventListener(`invalid`, onTitleInvalid);
+    title.addEventListener(`input`, onTitleInput);
 
-    const guestsNumber = adForm.querySelector(`#capacity`);
+    price.addEventListener(`input`, onPriceInput);
+
+    type.addEventListener(`change`, onTypeChange);
+
+    roomsNumber.addEventListener(`change`, onRoomsNumberChange);
     guestsNumber.addEventListener(`change`, onGuestNumberChange);
 
     enableFilters();
     setMainPinCoordinates();
 
     setCapacityValidity(roomsNumber);
+    setPricePlaceholder();
+    setPriceValidity(type);
   };
 
   const disable = () => {
     adForm.classList.add(`ad-form--disabled`);
 
-    const roomsNumber = adForm.querySelector(`#room_number`);
-    roomsNumber.removeEventListener(`change`, onRoomsNumberChange);
+    title.removeEventListener(`invalid`, onTitleInvalid);
+    title.removeEventListener(`input`, onTitleInput);
 
-    const guestsNumber = adForm.querySelector(`#capacity`);
+    price.removeEventListener(`input`, onPriceInput);
+
+    type.removeEventListener(`change`, onTypeChange);
+    price.removeEventListener(`change`, onPriceChange);
+
+    roomsNumber.removeEventListener(`change`, onRoomsNumberChange);
     guestsNumber.removeEventListener(`change`, onGuestNumberChange);
 
     disableFilters();
     setMainPinCoordinates();
   };
 
+  const setPricePlaceholder = () => {
+    price.placeholder = PRICE_RULES_MAP[type.value];
+  };
+
   const validateRoomCapacity = (rooms, guests) => CAPACITY_RULES_MAP[rooms].includes(guests);
 
   const setCapacityValidity = (target) => {
-    const roomsNumber = adForm.querySelector(`#room_number`);
-    const guestsNumber = adForm.querySelector(`#capacity`);
     const isValid = validateRoomCapacity(+roomsNumber.value, +guestsNumber.value);
 
     roomsNumber.setCustomValidity(``);
@@ -63,6 +88,60 @@
   const onGuestNumberChange = (evt) => {
     setCapacityValidity(evt.target);
   };
+
+  const onTitleInvalid = () => {
+    if (title.validity.tooShort) {
+      title.setCustomValidity(`Заголовок объявления должен состоять минимум из 30 символов`);
+    } else if (title.validity.tooLong) {
+      title.setCustomValidity(`Заголовок объявления не должен превышать 100 символов`);
+    } else if (title.validity.valueMissing) {
+      title.setCustomValidity(`Заголовок объявления это обязательное поле`);
+    } else {
+      title.setCustomValidity(``);
+    }
+  };
+
+  const onTitleInput = () => {
+    title.setCustomValidity(``);
+  };
+
+  const onPriceInput = () => {
+    setPriceValidity();
+  };
+
+  const onTypeChange = () => {
+    setPricePlaceholder();
+    setPriceValidity();
+  };
+
+  const onPriceChange = () => {
+    setPriceValidity();
+  };
+
+  const setPriceValidity = () => {
+
+    if (price.validity.valueMissing) {
+      price.setCustomValidity(`Цена за ночь это обязательное поле`);
+    } else if (price.validity.rangeOverflow) {
+      price.setCustomValidity(`Цена за ночь не должна превышать 1000000`);
+    }
+
+    if (price.validity.valueMissing || price.validity.rangeOverflow) {
+      return;
+    }
+
+    const isValid = validatePriceType(type.value, price.value);
+
+    if (!isValid) {
+      price.setCustomValidity(`Цена за ночь должна быть минимум ${PRICE_RULES_MAP[type.value]} для данного типа жилья`);
+    } else {
+      price.setCustomValidity(``);
+    }
+
+    price.reportValidity();
+  };
+
+  const validatePriceType = (typeValue, priceNumber) => PRICE_RULES_MAP[typeValue] <= priceNumber;
 
   const enableFilters = () => {
     for (const filter of adForm.children) {
