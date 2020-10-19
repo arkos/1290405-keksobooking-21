7003f8e9c1f1a473c5b6577e83265118049365ca
Kostya Kaganov <kkaganov@gmail.com>
2020-10-19 16:00:45 +0300

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    "./js/util.js",
+    "./js/decorator.js",
+    "./js/http.js",
+    "./js/pin.js",
+    "./js/card.js",
+    "./js/map.js",
+    "./js/form.js",
+    "./js/main.js",
+  ],
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname),
+    iife: true
+  },
+  devtool: false
+};
